c04e08531af6e29c124d66773fe1d14b61b9f526
Nemo <me@captnemo.in>
2024-06-28 10:53:38 +0530

    Drop non-bangalore events in townscript

--- src/townscript.py
+++ src/townscript.py
@@ -2,6 +2,12 @@ import yaml
 import json
 import http.client
 
+IGNORED_URL_SLUGS = [
+    'delhi',
+    'mumbai',
+    'pune',
+]
+
 def get_event_urls(org_id):
     conn = http.client.HTTPSConnection("www.townscript.com")
     conn.request("GET", f"/listings/event/upcoming/userId/{org_id}")
@@ -14,7 +20,9 @@ def main():
     config = yaml.safe_load(open('in/known-hosts.yml'))['townscript']
     for c in config:
         for e in get_event_urls(config[c]['id']):
-            print(e)
+            ignored = any(slug in e for slug in IGNORED_URL_SLUGS)
+            if ignored == False:
+                print(e)
 
 if __name__ == "__main__":
     main()
