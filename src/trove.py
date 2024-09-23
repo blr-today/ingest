b4db270cb020aed4a0c5f39e66cb47c9ebc74362
Nemo <me@captnemo.in>
2024-09-23 18:28:52 +0530

    refac: use shopify class to fetch trove events

--- src/trove.py
+++ src/trove.py
@@ -1,70 +1,45 @@
-from common.jsonld import JsonLdExtractor
 from bs4 import BeautifulSoup
-from datetime import timedelta, datetime
-from common.session import get_cached_session
 import datefinder
 import json
+
 from common.tz import IST
+from common import shopifyproducts
 
-BASE_URL = "https://troveexperiences.com/"
+BASE_URL = 'https://troveexperiences.com/'
+collection = 'collections/bangalore'
 
-# TODO: Move to TEST
-# Input dates
-dates = [
-    "Sat | Jun 29 | 10 AM - 12:30 PM",
-    "Sat | Jun 29 | 3 - 6 PM",
-    "Sun | Jun 30 | 9:30 -11:30 AM",
-    "Sun | Jun 30 | 11 AM - 1 PM",
-    "Sat | Jul 06 | 4 - 6:30 PM",
-    "Sun | Jul 07 | 8:30 AM to 11:30 AM",
-    "Sun | Jul 07 | 10 AM to 12:30 PM",
-    "Sun | Jul 14 | 10:30 AM - 1 PM",
-    "Sun | Jul 14 | 4 - 7 PM",
-]
+# Check product variants. If it is coming soon, there is no date, skip. Otherwise it will have date in title
+def filter_data(products):
+    data = []
+    for product in products:
+        for variant in product['variants']:
+            if variant['title'] != 'Coming Soon!':
+                data.append(product)
+                break
+    return data
 
-TIME_FORMAT_STRINGS = ["%I %p", "%I%p", "%I:%M %p", "%I:%M%p"]
+# Fetch offers from variants.
+def fetch_offers(product):
+    variants = product['variants']
+    offers = []
 
+    for variant in variants:
+        offer = {
+        '@type': 'Offer',
+        'priceCurrency': 'INR',
+        'price': variant['price'],
+        'sku': variant['sku'],
+        'url': f"{BASE_URL}{collection}/products/{product['handle']}",
+        }
 
-def scrape_trove(location):
-    session = get_cached_session()
+        offers.append(offer)
 
-    url = f"https://troveexperiences.com/collections/upcoming/{location}"
-    soup = BeautifulSoup(session.get(url).content, "html.parser")
-    for link in soup.select("a.grid-product__link"):
-        url = BASE_URL + link["href"]
-        r = session.get(url)
-        soup = BeautifulSoup(r.text, "html.parser")
-        date_str = soup.select_one('form div.variant-input input[type="radio"]')[
-            "value"
-        ]
-        startDate, endDate = parse_date(date_str)
-        location_text = soup.select_one(".custom-field__location").text
+    return offers
 
-        data = JsonLdExtractor().extract(r.text)
+# Fetch timings from the variant.title. It returns start_date and end_date timestamps
+def fetch_timings(variants):
+    date_str = variants[0]['title']
 
-        product = list(filter(lambda x: x["@type"] == "Product", data))[0]
-        if "InStock" in product["offers"][0]["availability"]:
-            yield {
-                "offers": product["offers"],
-                "name": product["name"],
-                "description": product["description"],
-                "url": product["url"],
-                "startDate": startDate.isoformat(),
-                "endDate": endDate.isoformat(),
-                "image": product["image"],
-                "location": location_text,
-            }
-
-
-def parse_time(time_str):
-    for fmt in TIME_FORMAT_STRINGS:
-        try:
-            return datetime.strptime(time_str, fmt).time()
-        except ValueError:
-            pass
-
-
-def parse_date(date_str):
     date_parts = date_str.split(" | ")
     # We use datefinder coz it finds the closest year automatically
     event_date = list(datefinder.find_dates(date_parts[1]))[0]
@@ -100,12 +75,35 @@ def parse_date(date_str):
 
     return timestamps
 
+def fetch_description(product):
+    description = BeautifulSoup(product['body_html'], 'html.parser').get_text()
+    description = description.replace('\u00a0', "\n")
+    description = description.replace('\u2014', '-')
+    description = description.replace('\u2019', "'")
+    return description
+
+def make_event(product):
+    start_date, end_date = fetch_timings(product['variants'])
+    duration = end_date.hour - start_date.hour
+
+    return {
+        "@context": "https://schema.org",
+        "@type": "Event",
+        'about': product['title'],
+        'description': fetch_description(product),
+        'url': f"{BASE_URL}{collection}/products/{product['handle']}",
+        'keywords': product['tags'],
+        'offers': fetch_offers(product),
+        'duration': duration,
+        'startDate': start_date.isoformat(),
+        'endDate': end_date.isoformat()
+    }
 
 if __name__ == "__main__":
-    events = []
-    for event in scrape_trove("bangalore"):
-        events.append(event)
+    sp = shopifyproducts.ShopifyProducts(BASE_URL, collection)
+    products = filter_data(sp.fetch_products())
+    
+    events = list(map(make_event, products))
     output_json_file = "out/trove.json"
     with open(output_json_file, "w") as f:
         json.dump(events, f, indent=2)
-    print(f"[TROVE] {len(events)} events")
