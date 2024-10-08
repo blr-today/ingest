5d141c8fb95655bb0e0b5792abc8e32d77ee437a
Nemo <me@captnemo.in>
2024-10-08 15:07:43 +0530

    ticketnew: formatting

--- src/ticketnew.py
+++ src/ticketnew.py
@@ -3,22 +3,15 @@ from common.session import get_cached_session
 from datetime import datetime
 import csv
 
-BASE_URL = 'https://apiproxy.paytm.com/v3/movies/search'
-MOVIE_LIST_PATH = '/movies'
-SHOWS_PATH = '/movie'
-CINEMA_PATH = '/cinemas'
+BASE_URL = "https://apiproxy.paytm.com/v3/movies/search"
+MOVIE_LIST_PATH = "/movies"
+SHOWS_PATH = "/movie"
+CINEMA_PATH = "/cinemas"
 
-QUERY_PARAMS = {
-    "city": "bengaluru"
-}
+QUERY_PARAMS = {"city": "bengaluru"}
 
 # Keys for csv files
-MOVIE_KEYS = [
-    "title",
-    "censorRating",
-    "runtime",
-    "movieId"
-]
+MOVIE_KEYS = ["title", "censorRating", "runtime", "movieId"]
 
 LANGUAGE_TO_ISO_MAP = {
     "Assamese": "as",
@@ -30,7 +23,7 @@ LANGUAGE_TO_ISO_MAP = {
     "Malayalam": "ml",
     "Marathi": "mr",
     "Tamil": "ta",
-    "Telugu": "te"
+    "Telugu": "te",
 }
 
 CINEMA_KEYS = [
@@ -50,7 +43,7 @@ SHOW_KEYS = [
     "screenName",
     "language",
     "startTime",
-    "endTime"
+    "endTime",
 ]
 
 
@@ -61,7 +54,8 @@ def make_request(session, path, query={}):
 
 def fetch_movie_list(session):
     res = make_request(session, MOVIE_LIST_PATH, {"mdp": "1"})
-    return res['data']['groupedMovies']
+    return res["data"]["groupedMovies"]
+
 
 # Codes are movie codes which coresponde to movie in a specific language. Required to fetch movie details
 
@@ -71,13 +65,15 @@ def fetch_movies_and_codes(movie_list):
     codes = []
     for movie_detail in movie_list:
         movie = {
-            'title': movie_detail['label'],
-            'movieId': movie_detail['contentId'],
-            'censorRating': movie_detail['censor'],
-            'runtime': movie_detail['duration'],
+            "title": movie_detail["label"],
+            "movieId": movie_detail["contentId"],
+            "censorRating": movie_detail["censor"],
+            "runtime": movie_detail["duration"],
         }
 
-        codes += list(map(lambda x: x['fmtGrpId'], movie_detail['languageFormatGroups']))
+        codes += list(
+            map(lambda x: x["fmtGrpId"], movie_detail["languageFormatGroups"])
+        )
 
         movies.append(movie)
 
@@ -88,26 +84,40 @@ def fetch_shows(session, codes):
     shows = []
 
     for code in codes:
-        show_dates = make_request(session, SHOWS_PATH, {"movieCode": code, "reqData": "1"})['data']['sessionDates']
+        show_dates = make_request(
+            session, SHOWS_PATH, {"movieCode": code, "reqData": "1"}
+        )["data"]["sessionDates"]
 
         # Fetch shows for all the available future dates
         for show_date in show_dates:
-            show_data = make_request(session, SHOWS_PATH, {"movieCode": code, "fromdate": show_date, "reqData": "1", "meta": "1"})
+            show_data = make_request(
+                session,
+                SHOWS_PATH,
+                {"movieCode": code, "fromdate": show_date, "reqData": "1", "meta": "1"},
+            )
 
-            shows_per_day = show_data['pageData']['sessions']
+            shows_per_day = show_data["pageData"]["sessions"]
 
             for cinema in shows_per_day.values():
                 for details in cinema:
-                    shows.append({
-                        'theatreId': details['cid'],
-                        'startTime': datetime.strptime(details['showTime'], "%Y-%m-%dT%H:%M").isoformat(),
-                        'endTime': datetime.strptime(details['closeTime'], "%Y-%m-%dT%H:%M").isoformat(),
-                        'availableSeats': details['avail'],
-                        'totalSeats': details['total'],
-                        'screenName': details['audi'],
-                        'movieId': show_data['meta']['movies'][0]['contentId'],
-                        'language': LANGUAGE_TO_ISO_MAP[show_data['meta']['movies'][0]['lang']]
-                    })
+                    shows.append(
+                        {
+                            "theatreId": details["cid"],
+                            "startTime": datetime.strptime(
+                                details["showTime"], "%Y-%m-%dT%H:%M"
+                            ).isoformat(),
+                            "endTime": datetime.strptime(
+                                details["closeTime"], "%Y-%m-%dT%H:%M"
+                            ).isoformat(),
+                            "availableSeats": details["avail"],
+                            "totalSeats": details["total"],
+                            "screenName": details["audi"],
+                            "movieId": show_data["meta"]["movies"][0]["contentId"],
+                            "language": LANGUAGE_TO_ISO_MAP[
+                                show_data["meta"]["movies"][0]["lang"]
+                            ],
+                        }
+                    )
 
     return shows
 
@@ -118,14 +128,17 @@ List of cinemas can be found using a single API call.
 
 
 def fetch_cinemas(session):
-    return [{'theatreId': cinema['id'],
-             'name': cinema['name'],
-             'cityname': cinema['city'],
-             'address': cinema['address'],
-             'latitude': cinema['lat'],
-             'longitude': cinema['lon']
-             } for cinema in make_request(session, CINEMA_PATH)['data']['cinemas']
-            ]
+    return [
+        {
+            "theatreId": cinema["id"],
+            "name": cinema["name"],
+            "cityname": cinema["city"],
+            "address": cinema["address"],
+            "latitude": cinema["lat"],
+            "longitude": cinema["lon"],
+        }
+        for cinema in make_request(session, CINEMA_PATH)["data"]["cinemas"]
+    ]
 
 
 def main():
@@ -140,22 +153,14 @@ def main():
     with (
         open("out/ticketnew-movies.csv", "w", newline="") as movies_file,
         open("out/ticketnew-sessions.csv", "w", newline="") as sessions_file,
-        open("out/ticketnew-cinemas.csv", "w", newline="") as cinemas_file
+        open("out/ticketnew-cinemas.csv", "w", newline="") as cinemas_file,
     ):
-        movie_writer = csv.DictWriter(
-            movies_file,
-            fieldnames=MOVIE_KEYS
-        )
+        movie_writer = csv.DictWriter(movies_file, fieldnames=MOVIE_KEYS)
 
-        session_writer = csv.DictWriter(
-            sessions_file,
-            fieldnames=SHOW_KEYS
-        )
+        session_writer = csv.DictWriter(sessions_file, fieldnames=SHOW_KEYS)
 
         cinema_writer = csv.DictWriter(
-            cinemas_file,
-            fieldnames=CINEMA_KEYS,
-            extrasaction='ignore'
+            cinemas_file, fieldnames=CINEMA_KEYS, extrasaction="ignore"
         )
 
         movie_writer.writeheader()
@@ -171,5 +176,5 @@ def main():
     print(f"[TicketNew] {len(movies)} movies")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
