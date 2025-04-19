7d5b046f1a1652dfde969fbdc3cc7a9e7c6a123b
Nemo <me@captnemo.in>
2025-04-19 20:03:53 +0530

    SIS fixes

--- src/sis.py
+++ src/sis.py
@@ -10,56 +10,29 @@ import datefinder
 
 BASE_URL = "https://sistersinsweat.in"
 
-
-def fetch_events_html(session):
-    response = session.get(BASE_URL + "/events?city=4")
-    return response.text
-
-
 def fetch_sessions_html(session):
-    response = session.get(BASE_URL + "/sessions?city=4&Sport=All")
+    body = """{
+      "City": "BENGALURU"
+    }
+    """
+    response = session.post(BASE_URL + "/filter-events", data=body, headers={
+        "content-type": "application/json"
+    })
     return response.text
 
-
-def fix_date(date_str):
-    return date_str.replace(" ", "T") + "+05:30"
-
-
-def fetch_ajax_details(sku, _token):
-    data = {"_token": _token, "sku": escape(sku, quote=False), "location_name": "4"}
-    url = BASE_URL + "/getTimeSlotAjax"
-    e = session.post(url, data=json.dumps(data), headers={"content-type": "application/json"}).json()
-    try:
-        e = e[0]
-    except IndexError:
-        print("[SIS] Failed to get schedule for " + sku)
+def make_event_details(soup):
+    l = soup.select_one(".box-button a").get("href")
+    if "package-detail" in l:
         return None
-
-    # The end_date is unreliable. The website uses the date from the start_date
-    # and the time from the end_date as the actual endsAt
-
-    endsAt = fix_date(e["start_date"].split(" ")[0] + " " + e["end_date"].split(" ")[1])
-
-    return {
-        "startDate": fix_date(e["start_date"]),
-        "endDate": endsAt,
-        "offers": [{"price": e["price"], "priceCurrency": "INR"}],
-    }
-
-
-def fetch_event_details(session, l):
-    sport = l.split("/")[4]
-    if sport == "packages":
-        return None
-    if sport == "yoga":
+    if "Yoga" in l:
         event_type = "SocialEvent"
+    elif "Pizza" in l or "cooking" in l:
+        event_type = "FoodEvent"
     else:
         event_type = "SportsEvent"
-    response = session.get(l)
-    html = response.text
-    soup = BeautifulSoup(html, "html.parser")
-    div = soup.select_one("#description")
-    title = soup.select_one("h2").text
+    description = f"See more details at {BASE_URL + l}"
+    # Remove the price from the description
+    title = soup.select_one(".title").text.strip()
     # Not an event
     NOT_EVENT = ["subscription", "package"]
     for word in NOT_EVENT:
@@ -68,7 +41,7 @@ def fetch_event_details(session, l):
     event = {
         "name": title,
         "@type": event_type,
-        "url": l,
+        "url": BASE_URL + l,
         "@context": "https://schema.org",
         "keywords": ["SISTERSINSWEAT"],
         "audience": {"@type": "Audience", "AudienceType": "women"},
@@ -83,19 +56,18 @@ def fetch_event_details(session, l):
     }
     # Since SISTERSINSWEAT events can be non-sport events as well
     # we add a secondary tag as well to help with filtering
-    if sport in ["basketball", "football", "badminton", "running", "yoga", "pickleball"]:
+    if event_type == "SportsEvent":
         event["keywords"].append("SISTERSINSWEAT/SPORTS")
-        event['sport'] = sport.title()
+        event['sport'] = l.split("-")[3]
     else:
         event["keywords"].append("SISTERSINSWEAT/SESSION")
 
-    if div:
-        text = div.text.replace("\n \n", "\n")
-        event["description"] = text.strip()
-    img = soup.select_one("img.img-fluid")
+    event["description"] = description.replace("\n \n", "\n").strip()
+    
+    img = soup.select_one(".box-image img")
     if img:
         event["image"] = img.get("src")
-    v = soup.select_one("#venue")
+    v = soup.select_one(".box-city h3")
     # Ignore virtual events
     if v:
         if "Virtual" in v.text:
@@ -105,37 +77,41 @@ def fetch_event_details(session, l):
             venue_text = "Cubbon Park"
             venue_address = "Kasturba Road, Bangalore"
         else:
-            venue_text = venue_text.replace("\r", "")
-            venue_text = venue_text.replace(",", "\n")
-            s = venue_text.split("\n")
-            venue_text = s[0].strip()
-            venue_address = ", ".join(s[1:])
+            venue_address = v.find_next("p").text
+            # Drop all text after ", Bengaluru"
+            if ", Bengaluru" in venue_address:
+                venue_address = venue_address.split(", Bengaluru")[0]
         event["location"] = {
             "name": venue_text.strip(),
             "address": venue_address.strip(),
             "type": "Place",
         }
-    sku = soup.select_one('input[name="sku"]').get("value")
-    _token = soup.select_one('input[name="_token"]').get("value")
-    event = event | fetch_ajax_details(sku, _token)
-    if 'startDate' not in event:
+    date = soup.select_one(".box-date")
+    if date:
+        date_text = date.select_one(".box-date .date-text3").text
+        # Remove the time from the date text
+        date_text = re.sub(r"\d{1,2}:\d{2} [AP]M", "", date_text).strip()
+        # Find the date in the text
+        matches = list(datefinder.find_dates(date_text))
+        if matches:
+            event["startDate"] = matches[0].replace(tzinfo=IST).isoformat()
+            event["endDate"] = (matches[0] + datetime.timedelta(hours=1)).replace(tzinfo=IST).isoformat()
+    else:
+        print("No date found " + l)
         return None
     return event
 
 
-def fetch_events_links(html):
+def fetch_event_boxes(html):
     soup = BeautifulSoup(html, "html.parser")
-    links = soup.select('a[href^="https://sistersinsweat.in/session_details/"]')
-    return [link.get("href") for link in links]
-
+    return soup.select('.list-view-box')
 
 if __name__ == "__main__":
     session = requests.Session()
-    html = fetch_events_html(session)
-    html2 = fetch_sessions_html(session)
+    html = fetch_sessions_html(session)
     events = []
-    for l in fetch_events_links(html) + fetch_events_links(html2):
-        e = fetch_event_details(session, l)
+    for box_soup in fetch_event_boxes(html):
+        e = make_event_details(box_soup)
         if e:
             events.append(e)
     with open("out/sis.json", "w") as f:
