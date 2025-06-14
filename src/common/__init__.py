8c8b9834c702b8c25001c5b2137f807d48f0aa5b
Nemo <commits@captnemo.in>
2025-06-14 21:33:27 +0530

    upgrade firefox version in UA header

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -1,3 +1,3 @@
 USER_AGENT_HEADERS = {
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0 blr-today-fetcher/0 (+https://blr.today/docs/bots)"
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0 blr-today-fetcher/0 (+https://blr.today/docs/bots)"
 }
