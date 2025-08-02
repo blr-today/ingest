b9351c41a1e0d2274751472f306dad69898e4c92
Nemo <commits@captnemo.in>
2025-08-02 23:26:24 +0530

    [SKILLBOXES] Don't crash on some weird parent-child events, and only fetch details for events in BLR

--- src/skillboxes.py
+++ src/skillboxes.py
@@ -7,12 +7,14 @@ import sys
 BASE_URL = "https://www.skillboxes.com/servers"
 HEADERS = {
     "Content-Type": "application/json",
-    "User-Agent": "Mozilla/5.0 (compatible; blr.today-bot; +httpss://blr.today/bot/)"
+    "User-Agent": "Mozilla/5.0 (compatible; blr.today-bot; +httpss://blr.today/bot/)",
+    "Accept": "application/json"
 }
 
 def get_events(city, page=1):
     payload = {
-        "city": city,
+        "default_city": city,
+        "opcode": "search",
         "page": page,
         "type": "fetchAll"
     }
@@ -21,36 +23,49 @@ def get_events(city, page=1):
         "https://www.skillboxes.com/servers/v3/api/event-new/get-event",
         json=payload,
         headers=HEADERS,
-    ).json()
-    for item in res["items"]:
+    )
+    if (res.status_code != 200):
+        print("[SKILLBOXES] Failed to get list of events.", file=sys.stderr)
+        sys.exit(0)
+    data = res.json()
+    for item in data["items"]:
         yield item['slug']
-    if res["next"]:
+    if data["next"]:
         yield from get_events(city, page + 1)
 
 def get_event_details(session, slug):
     payload = {"slug": slug}
-    details = session.post(
+    res = session.post(
         "https://www.skillboxes.com/servers/v3/api/event-new/event-details",
         json=payload,
         headers=HEADERS,
-    ).json()['data']
+    )
 
+    if (res.status_code != 200):
+        print(f"[SKILLBOXES] Failed to get details for event {slug}.", file=sys.stderr)
+        return None
+
+    return res.json()['data']
+
+def get_event_tickets(session, slug):
     try:
-        r = session.post(
+        payload = {"slug": slug}
+        res = session.post(
             "https://www.skillboxes.com/servers/v3/api/event-new/event-tickets",
             json=payload,
             headers=HEADERS,
         ).json()
 
-        if r['success'] == False:
-            return None
-        details['tickets'] = r['data']
-        return details
+        if res['success'] == False:
+            return []
+        
+        return res['data']
     except Exception as e:
         if "more than 100 headers" in str(e):
-            return None
+            return []
         else:
-            raise e
+            print(f"[SKILLBOXES] Error fetching tickets for event {slug}: {e}", file=sys.stderr)
+            return []
 
 
 def __main__(cities):
@@ -59,7 +74,8 @@ def __main__(cities):
     for city in cities:
         for slug in get_events(city):
             details = get_event_details(session, slug)
-            if details:
+            if details and details['city_id'] == city:
+                details['tickets'] = get_event_tickets(session, slug)
                 events.append(details)
 
     with open("out/skillboxes.jsonnet", "w") as f:
