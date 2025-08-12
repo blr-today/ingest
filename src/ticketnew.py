678da600652d1e9ebd53b42e3d342da9656b8494
Nemo <me@captnemo.in>
2025-08-12 12:58:50 +0530

    ticketnew: write movies as JSON

--- src/ticketnew.py
+++ src/ticketnew.py
@@ -12,7 +12,8 @@ CINEMA_PATH = "/cinemas"
 QUERY_PARAMS = {"city": "bengaluru"}
 
 # Keys for csv files
-MOVIE_KEYS = ["title", "censorRating", "runtime", "movieId"]
+MOVIE_KEYS = ["title", "censorRating", "runtime", "movieId", "genres", "image"]
+MOVIE_SCREEN_FORMATS = {}
 
 LANGUAGE_TO_ISO_MAP = {
     "Assamese": "as",
@@ -55,6 +56,8 @@ SHOW_KEYS = [
     "language",
     "startTime",
     "endTime",
+    "format",
+    "availability"
 ]
 
 
@@ -77,7 +80,7 @@ def fetch_movie_list(session):
 # Codes are movie codes which coresponde to movie in a specific language. Required to fetch movie details
 
 
-def fetch_movies_and_codes(movie_list):
+def fetch_movies(movie_list):
     movies = []
     codes = []
     for movie_detail in movie_list:
@@ -86,24 +89,49 @@ def fetch_movies_and_codes(movie_list):
             "movieId": movie_detail["contentId"],
             "censorRating": movie_detail["censor"],
             "runtime": movie_detail["duration"],
+            "genres": ", ".join(movie_detail["grn"]),
+            "image": movie_detail["appImgPath"],
+            "formats": []
         }
 
-        codes += list(
-            map(lambda x: x["fmtGrpId"], movie_detail["languageFormatGroups"])
-        )
+        for lang in movie_detail["languageFormatGroups"]:
+            movie['formats'].append({
+                "lang": LANGUAGE_TO_ISO_MAP[lang['lang']],
+                "id": lang['fmtGrpId'],
+                "screenFormats": []
+            })
+            for screenFormat in lang['screenFormats']:
+                movie['formats'][-1]['screenFormats'].append({
+                    "id": screenFormat['movieCode'],
+                    "name": screenFormat['scrnFmt'],
+                    "earliestDate": screenFormat['nextAvailableDate'],
+                })
 
         movies.append(movie)
 
-    return [movies, codes]
+    return movies
 
-def parse_show_data(shows_per_day, shows, movieInfo):
+
+def parse_show_data(shows_per_day, shows, movie):
+    def reverse_lookup_format(mid):
+        for formatData in movie["formats"]:
+            for screenFormat in formatData["screenFormats"]:
+                if screenFormat["id"] == mid:
+                    return screenFormat["name"]
+        return None
+
+    def reverse_lookup_lang(fid):
+        for formatData in movie["formats"]:
+            if formatData["id"] == fid:
+                return formatData["lang"]
+        return None
     for cinema in shows_per_day.values():
         for details in cinema:
-            language = movieInfo["lang"]
-            if language not in LANGUAGE_TO_ISO_MAP:
-                print(f"[TICKETNEW] Unknown language: {language}", file=sys.stderr)
-            shows.append(
-                {
+            # print(details)
+            language = reverse_lookup_lang(details['fid'])
+            availability = sum(area['sAvail'] for area in details['areas'])
+            if availability > 0:
+                shows.append({
                     "theatreId": details["cid"],
                     "startTime": datetime.strptime(
                         details["showTime"], "%Y-%m-%dT%H:%M"
@@ -114,40 +142,42 @@ def parse_show_data(shows_per_day, shows, movieInfo):
                     "availableSeats": details["avail"],
                     "totalSeats": details["total"],
                     "screenName": details["audi"],
-                    "movieId": movieInfo["contentId"],
-                    "language": LANGUAGE_TO_ISO_MAP.get(
-                        language,
-                        language
-                    )
-                }
-            )
+                    "movieId": movie["movieId"],
+                    "format": reverse_lookup_format(details['mid']),
+                    "language": language,
+                    "availability": availability
+                })
 
-def fetch_shows(session, codes):
+
+def fetch_shows(session, movies):
     shows = []
 
-    for code in codes:
-        res = make_request(
-            session, SHOWS_PATH, {"movieCode": code, "reqData": "1"}
-        )
-        # Movie is not playing really
-        if 'sessionDates' in res['data']:
-            for show_date in res["data"]["sessionDates"]:
-                res = make_request(
-                    session,
-                    SHOWS_PATH,
-                    {"movieCode": code, "fromdate": show_date, "reqData": "1", "meta": "1"},
-                )
+    for movie in movies:
+        for formatData in movie["formats"]:
+            code = formatData["id"]
+            res = make_request(
+                session, SHOWS_PATH, {"movieCode": code, "reqData": "1"}
+            )
+            # Movie is not playing really
+            if 'sessionDates' in res['data']:
+                for show_date in res["data"]["sessionDates"]:
+                    res = make_request(
+                        session,
+                        SHOWS_PATH,
+                        {"movieCode": code, "date": show_date, "reqData": "1", "meta": "1"},
+                    )
 
-                try:
-                    shows_per_day = res["pageData"]["sessions"]
-                    movieInfo = res['meta']['movies'][0]
-                    parse_show_data(shows_per_day, shows, movieInfo)
-                except KeyError as e:
-                    print(res.keys())
-                    print(res)
-                    shows_per_day = []
-                    continue
-        
+                    try:
+                        shows_per_day = res["pageData"]["sessions"]
+                        movieInfo = res['meta']['movies'][0]
+                        parse_show_data(shows_per_day, shows, movie)
+                    except KeyError as e:
+                        print(e)
+                        print(res.keys())
+                        print(res)
+                        shows_per_day = []
+                        continue
+
     return shows
 
 
@@ -170,33 +200,46 @@ def fetch_cinemas(session):
     ]
 
 
+"""
+Update shows so it only has shows with unique
+movieId,theatreId,screenName,language,startTime combination
+combination
+"""
+
+
+def unique_shows(show):
+    unique = {}
+    for s in show:
+        key = (s["movieId"], s["theatreId"], s["screenName"], s["language"], s["startTime"])
+        if key not in unique:
+            unique[key] = s
+    return list(unique.values())
+
+
 def main():
     session = get_cached_session()
     cinemas = fetch_cinemas(session)
 
     movie_list = fetch_movie_list(session)
-    movies, codes = fetch_movies_and_codes(movie_list)
+    movies = fetch_movies(movie_list)
 
-    shows = fetch_shows(session, codes)
+    shows = fetch_shows(session, movies)
 
     with (
-        open("out/ticketnew-movies.csv", "w", newline="") as movies_file,
+        open("out/ticketnew-movies.json", "w", newline="") as movies_file,
         open("out/ticketnew-sessions.csv", "w", newline="") as sessions_file,
         open("out/ticketnew-cinemas.csv", "w", newline="") as cinemas_file,
     ):
-        movie_writer = csv.DictWriter(movies_file, fieldnames=MOVIE_KEYS)
-
         session_writer = csv.DictWriter(sessions_file, fieldnames=SHOW_KEYS)
 
         cinema_writer = csv.DictWriter(
             cinemas_file, fieldnames=CINEMA_KEYS, extrasaction="ignore"
         )
 
-        movie_writer.writeheader()
-        movie_writer.writerows(movies)
+        movies_file.write(json.dumps(movies, indent=4))
 
         session_writer.writeheader()
-        session_writer.writerows(shows)
+        session_writer.writerows(unique_shows(shows))
 
         cinema_writer.writeheader()
         cinema_writer.writerows(cinemas)
